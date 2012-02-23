3b44373b425c2f30c5abd948827a924731505d72
Colin Edwards <colin@recursivepenguin.com>
2012-02-23 01:10:33 -0600

    Access token can be fetched from Oauth

--- ioio-github-bell/src/com/recursivepenguin/android/ioiogithubbell/OauthActivity.java
+++ ioio-github-bell/src/com/recursivepenguin/android/ioiogithubbell/OauthActivity.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -55,32 +58,47 @@ public class OauthActivity extends Activity {
 		}
 	};
 
-	class FetchAccessToken extends AsyncTask<String, Object, Object> {
+	public static Map<String, String> getQueryMap(String query) {
+		String[] params = query.split("&");
+		Map<String, String> map = new HashMap<String, String>();
+		for (String param : params) {
+			String name = param.split("=")[0];
+			String value = param.split("=")[1];
+			map.put(name, value);
+		}
+		return map;
+	}
+
+	class FetchAccessToken extends AsyncTask<String, Object, String> {
 
 		@Override
-		protected Object doInBackground(String... code) {
-			
+		protected String doInBackground(String... code) {
+
 			if (code.length > 0) {
-			
-				HttpPost post = new HttpPost("https://github.com/login/oauth/access_token");
-				HttpParams params = new BasicHttpParams();
-				params.setParameter("client_id", Common.CLIENT_ID);
-				params.setParameter("client_secret", Common.CLIENT_SECRET);
-				params.setParameter("code", code[0]);
-				post.setParams(params);
-				
+
+				String postUrl = "https://github.com/login/oauth/access_token";
+				postUrl += "?client_id=" + Common.CLIENT_ID;
+				postUrl += "&client_secret=" + Common.CLIENT_SECRET;
+				postUrl += "&code=" + code[0];
+
+				HttpPost post = new HttpPost(postUrl);
+
 				try {
 					HttpResponse response = mClient.execute(post);
 					HttpEntity entity = response.getEntity();
 					InputStream inputStream = entity.getContent();
-					BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
-					StringBuilder total = new StringBuilder(inputStream.available());
+					BufferedReader r = new BufferedReader(
+							new InputStreamReader(inputStream));
+					StringBuilder total = new StringBuilder(
+							inputStream.available());
 					String line;
 					while ((line = r.readLine()) != null) {
-					    total.append(line);
+						total.append(line);
 					}
-					
-					Log.d("", total.toString());
+
+					String result = total.toString();
+					Map<String, String> data = getQueryMap(result);
+					return data.get("access_token");
 				} catch (ClientProtocolException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -89,8 +107,17 @@ public class OauthActivity extends Activity {
 					e.printStackTrace();
 				}
 			}
-			
+
 			return null;
 		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (result != null) {
+				Log.d("", result);
+			}
+		}
+		
+		
 	}
 }
